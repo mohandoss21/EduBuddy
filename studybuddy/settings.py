0cb287664a1099dbe75a177188c3960e5901d1c8
mohandoss21 <MohandossV@4146c987a9a6>
2024-07-16 16:27:17 +0000

    d

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)@--i@y@g)!9epwv3ybwt)hgz^$oojldbz709*-6phw^-ck$u*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['MohandossV.pythonanywhere.com']
 
 
 # Application definition
