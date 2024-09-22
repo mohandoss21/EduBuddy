74d66665b3bc18a0a3487933ba8dacaaf0480da2
mohandoss21 <mohandossvelmurugan21@gmail.com>
2024-09-23 01:07:46 +0530

    Message ordering

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)@--i@y@g)!9epwv3ybwt)hgz^$oojldbz709*-6phw^-ck$u*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['MohandossV.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','MohandossV.pythonanywhere.com']
 
 #ASGI_APPLICATION = 'studybuddy.asgi.application'
 
